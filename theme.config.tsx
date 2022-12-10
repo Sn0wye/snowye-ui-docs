aece868ce259c47d74f4bba44ebc45dfec46fd4b
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-12-10 19:20:49 -0300

    feat: add footer and head tags

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,4 +1,5 @@
-import { DocsThemeConfig } from 'nextra-theme-docs';
+import { useRouter } from 'next/router';
+import { DocsThemeConfig, useConfig } from 'nextra-theme-docs';
 
 const config: DocsThemeConfig = {
   logo: (
@@ -12,7 +13,15 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/Sn0wye/snowye-ui/tree/main/docs',
   footer: {
-    text: 'Snowye UI'
+    text: (
+      <span>
+        MIT {new Date().getFullYear()} ©{' '}
+        <a href='https://snowye.dev' target='_blank'>
+          Gabriel Trzimajewski
+        </a>
+        .
+      </span>
+    )
   },
   darkMode: false,
   nextThemes: {
@@ -22,6 +31,26 @@ const config: DocsThemeConfig = {
     return {
       titleTemplate: '%s - Snowye UI'
     };
+  },
+  head: () => {
+    const { asPath } = useRouter();
+    const { frontMatter } = useConfig();
+    return (
+      <>
+        <meta
+          property='og:url'
+          content={`https://snowye-ui.snowye.dev${asPath}`}
+        />
+        <meta property='og:title' content={frontMatter.title || 'Snowye UI'} />
+        <meta
+          property='og:description'
+          content={
+            frontMatter.description ||
+            'React Powered Component UI, accessible by default and built on top of Radix.'
+          }
+        />
+      </>
+    );
   }
 };
 

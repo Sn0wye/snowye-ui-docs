78ec5f40169b107d77db741c41bc48330a81bb55
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-12-10 14:03:30 -0300

    feat: update button page

--- src/demos/ButtonDemo.tsx
+++ src/demos/ButtonDemo.tsx
@@ -1,46 +1,5 @@
-/** @jsxImportSource react */
-
 import { Button } from '@snowye-ui/react';
-import { styled } from '../styles/stitches';
-
-const Flex = styled('div', {
-  display: 'flex',
-  gap: '$2'
-});
 
 export const ButtonDemo = () => {
-  return (
-    <Flex
-      css={{
-        flexDirection: 'column',
-        gap: '$4'
-      }}
-    >
-      <Flex
-        css={{
-          justifyContent: 'center'
-        }}
-      >
-        <Button variant='primary'>Primary</Button>
-        <Button variant='secondary'>Secondary</Button>
-        <Button variant='tertiary'>Tertiary</Button>
-        <Button variant='danger'>Danger</Button>
-      </Flex>
-      <Flex
-        css={{
-          justifyContent: 'center'
-        }}
-      >
-        <Button size='sm'>Small</Button>
-        <Button size='md'>Medium</Button>
-        <Button size='lg'>Large</Button>
-        <Button loading>Full</Button>
-        <Button disabled>Disabled</Button>
-      </Flex>
-
-      <Button full>Full</Button>
-    </Flex>
-  );
+  return <Button>Hello World!</Button>;
 };
-
-export default ButtonDemo;
